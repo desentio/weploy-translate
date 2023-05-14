3ce405846ab15b384a311ae65c4a50c1cbb532ae
Lukas Seyfarth <luki.seyfarth@web.de>
2023-05-14 20:52:51 +0200

    Added MutationObserver

--- index.js
+++ index.js
@@ -13,15 +13,12 @@
 
 function saveLanguageToLocalStorage() {
   var language = navigator.language || navigator.userLanguage; // Get browser language
-  // var language = (navigator.language || navigator.userLanguage).substr(0, 2); // Get browser language
-  console.log("language in local storage:", language);
   // Save the language to local storage
   localStorage.setItem("language", language);
 }
 
 function getLanguageFromLocalStorage() {
   const language = localStorage.getItem("language");
-  console.log("language in local storage:", language);
   return language; // Get the language from local storage
 }
 
@@ -46,45 +43,15 @@ function getTranslationsFromAPI(strings, language, apiKey) {
 
 function hasTextNodes(node) {
   for (let child of node.childNodes) {
-      if (child.nodeType === Node.TEXT_NODE) {
-          return true;
-      }
-
-      if (child.nodeType === Node.ELEMENT_NODE && hasTextNodes(child)) {
-          return true;
-      }
+    if (child.nodeType === Node.TEXT_NODE) {
+      return true;
+    }
+    if (child.nodeType === Node.ELEMENT_NODE && hasTextNodes(child)) {
+      return true;
+    }
   }
-
   return false;
 }
-
-
-// function getTranslation(payloadChunks, language) {
-
-//   let resultChunks = []
-
-//   return new Promise((resolve) => {
-//     console.log("payload:", {
-//       chunkInputSize: payloadChunks.length,
-//       language: language,
-//       payload: payloadChunks,
-//     })
-
-//     payloadChunks.forEach(async (chunk) => {
-//       const result = await getTranslationsFromAPI(chunk, language)
-//       resultChunks.push(result)
-//       console.log("Chunk resived:", {
-//         inputChunkSize: resultChunks.length,
-//         resultChunkSize: result.length,
-//         language: language,
-//         result: resultChunks,
-//       })
-//       if (resultChunks.length === payloadChunks.length) {
-//         resolve(resultChunks)
-//       }
-//     })
-//   })
-// }
 
 function extractTextNodes(node, textNodes) {
   console.log("node:", node)
@@ -105,6 +72,7 @@ function filterValidTextNodes(textNodes) {
 }
 
 function processTextNodes(textNodes, language, apiKey) {
+  return new Promise(async (resolve, reject) => {
   // remove empty string
   const cleanTextNodes = textNodes.filter(
     (textNode) =>
@@ -115,102 +83,120 @@ function processTextNodes(textNodes, language, apiKey) {
   const textNodesTextContent = cleanTextNodes.map(
     (textNode) => textNode.textContent
   );
-  console.log("textNodesTextContent:", textNodesTextContent);
 
   //PROBLEM: THE NODES NEED TO COME BACK IN THE SAME ORDER AS THEY WERE SENT!!!!
   getTranslationsFromAPI(textNodesTextContent, language, apiKey).then(
     (response) => {
-      console.log("response:", response);
       // make a for loop to replace textNodes textContent with the response
       response.forEach((chunk, index) => {
-        console.log("chunk:", chunk, index, cleanTextNodes[index].textContent);
         cleanTextNodes[index].textContent = chunk;
-      });
+      })
+      resolve()
     }
   );
+  })
 
   // for (let textNode of textNodes) {
   //   textNode.textContent = processFunction(textNode.textContent);
   // }
 }
 
-async function modifyHtmlStrings(rootElement, language, apiKey) {
-  console.log("rootElement:", rootElement)
-  const textNodes = [];
-  extractTextNodes(rootElement, textNodes);
-  console.log("textNodes:", textNodes)
-  const validTextNodes = filterValidTextNodes(textNodes);
-  console.log("textNodes:", validTextNodes)
-  processTextNodes(validTextNodes, language, apiKey);
+function modifyHtmlStrings(rootElement, language, apiKey) {
+  return new Promise(async (resolve, reject) => {
+    const textNodes = [];
+    extractTextNodes(rootElement, textNodes);
+
+    const validTextNodes = filterValidTextNodes(textNodes);
+    await processTextNodes(validTextNodes, language, apiKey);
+
+    resolve();
+  })
 }
 
 function checkForWeployExcludeClasses(rawHTML) {
   // Clone the element to not change the original DOM
-  const elementsToExclude = rawHTML.querySelectorAll('.weploy-exclude')
-  
-  elementsToExclude.forEach((element) => {
-      element.parentNode.removeChild(element);
+  const elementsToExclude = Array.from(rawHTML.querySelectorAll('.weploy-exclude'))
+  const parentsAndIndexes = elementsToExclude.map((element) => {
+    const parent = element.parentNode;
+    const index = Array.from(parent.children).indexOf(element);
+    return { element, parent, index };
   });
-  
-  // Convert the element back to a string
-  const filteredHtml = rawHTML;
-  console.log("filteredHtml:", filteredHtml)
-  return filteredHtml;
+
+  elementsToExclude.forEach((element) => {
+    element.parentNode.removeChild(element);
+  });
+
+  return { filteredHtml: rawHTML, parentsAndIndexes };
+}
+
+async function startTranslationCycle(node, apiKey, observer) {
+  observer.disconnect();
+  console.log("Translation cycle START")
+
+  const { filteredHtml, parentsAndIndexes } = checkForWeployExcludeClasses(node)
+
+  await modifyHtmlStrings(filteredHtml, getLanguageFromLocalStorage(), apiKey)
+
+  parentsAndIndexes.forEach(({ element, parent, index }) => {
+    parent.insertBefore(element, parent.children[index]);
+  });
+  console.log("Translation cycle END")
+  observer.observe(document, { childList: true, subtree: true, characterData: true });
 }
 
 
 function getTranslations(apiKey) {
   //TODO check if language is set in localstorage 
-  const observer = new MutationObserver(mutations => {
+  let observer = new MutationObserver(mutations => {
     // Iterate over all mutations that just occured
-    for(let mutation of mutations) {
-        // If the mutation was the addition of nodes
-        if(mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-            for(let node of mutation.addedNodes) {
-                // If the node or any of its children are text nodes, log it
-                if (hasTextNodes(node)) {
-                    modifyHtmlStrings(checkForWeployExcludeClasses(node), getLanguageFromLocalStorage(), apiKey)
-                    console.log(node.outerHTML);
-                }
-            }
+    for (let mutation of mutations) {
+      // If the mutation was the addition of nodes
+      if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+        for (let node of mutation.addedNodes) {
+          // If the node or any of its children are text nodes, log it
+          if (hasTextNodes(node)) {
+            console.log(node.outerHTML);
+            startTranslationCycle(node, apiKey, observer)
+          }
         }
+      }
+
+      // If the mutation was a change in text content
+      if (mutation.type === 'characterData') {
+        console.log(mutation.target.parentElement)
+        startTranslationCycle(mutation.target.parentElement, apiKey, observer)
+        // startTranslationCycle(mutation.target.parentElement, apiKey)
+      }
     }
+    // observer.observe(document, { childList: true, subtree: true, characterData: true });
   });
-  
+
   //If no lang set check browser setting and use that 
-  const observerConfig = { childList: true, subtree: true };
+
   initalRawHTML = document.getElementById('weploy-translate');
 
-  observer.observe(initalRawHTML, observerConfig);
-  const pageHtml = checkForWeployExcludeClasses(initalRawHTML)
-  console.log("getLanguageFromLocalStorage()", pageHtml)
+
+
   if (getLanguageFromLocalStorage() === null) {
     saveLanguageToLocalStorage()
   }
 
-  modifyHtmlStrings(pageHtml, getLanguageFromLocalStorage(), apiKey)
+  startTranslationCycle(initalRawHTML, apiKey, observer)
 }
 
 
 function switchLanguage(language) {
-  //set new language in local storage
   localStorage.setItem('language', language)
   setTimeout(() => {
     location.reload()
   }, 1000)
-
-  //reload page
 }
-
 
 function getSelectedLanguage() {
   return new Promise((resolve, reject) => {
     let language = localStorage.getItem('language');
     if (language) {
-      console.log('Language found in local storage:', language);
       resolve(language); // Resolve the promise
-    } else {
-      console.log('Language not found in local storage.');
     }
   })
 }
