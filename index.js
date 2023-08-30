81e052947d1dab8433577e64d72674ac69a359a9
jemikanegara <jemikanegara@gmail.com>
2023-08-30 23:07:32 +0800

    inject onchange into lang select

--- index.js
+++ index.js
@@ -300,7 +300,7 @@ async function createLanguageSelect(apiKey) {
     const selectElem = document.createElement('select');
     selectElem.className = "weploy-exclude";
     selectElem.style = "border-radius: 9999px; border: none; background: transparent; color: #808080; cursor: pointer;";
-
+    selectElem.onchange = (e) => switchLanguage(e.target.value)
 
     // Assuming availableLangs array is available in this scope
     availableLangs.forEach(lang => {
