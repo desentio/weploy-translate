2c4854c69280209d3b7ec747b196b3b02a7bbde0
Lukas Seyfarth <luki.seyfarth@web.de>
2023-11-12 15:43:27 +0100

    fixed on white select

--- index.js
+++ index.js
@@ -267,7 +267,7 @@ async function createLanguageSelect(apiKey) {
     // Create the select element
     const selectElem = document.createElement('select');
     selectElem.className = "weploy-exclude";
-    selectElem.style = "text-transform: uppercase; border: none; background: transparent; cursor: pointer; outline: none;";
+    selectElem.style = "text-transform: uppercase; border: none; background-color: transparent; cursor: pointer; outline: none;";
     selectElem.onchange = (e) => switchLanguage(e.target.value)
 
     // Assuming availableLangs array is available in this scope
