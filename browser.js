3ddcce1504e2763e7f52b9bc057d7f701ebd9324
Jemika Negara <42384614+jemikanegara@users.noreply.github.com>
2023-11-20 21:15:35 +0800

    fix script tag

--- browser.js
+++ browser.js
@@ -1,10 +1,10 @@
 const { getTranslations, isBrowser, createLanguageSelect } = require("./index.js");
 
 if (isBrowser) {
+  var weployScriptTag = document.currentScript;
   document.addEventListener("DOMContentLoaded", function() {
-    const scriptTag = document.currentScript;
-    const apiKey = scriptTag.getAttribute("data-weploy-key");
-    const disableAutoTranslateAttr = scriptTag.getAttribute("data-disable-auto-translate")
+    const apiKey = weployScriptTag.getAttribute("data-weploy-key");
+    const disableAutoTranslateAttr = weployScriptTag.getAttribute("data-disable-auto-translate")
     const disableAutoTranslate = disableAutoTranslateAttr == "true";
     getTranslations(apiKey, {disableAutoTranslate});
     createLanguageSelect(apiKey);
