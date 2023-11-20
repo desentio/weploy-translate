7d0d777650bf6604e4fe22ab618b8657b5218551
Jemika Negara <42384614+jemikanegara@users.noreply.github.com>
2023-11-20 12:27:25 +0800

    on dom loaded

--- browser.js
+++ browser.js
@@ -1,10 +1,12 @@
 const { getTranslations, isBrowser, createLanguageSelect } = require("./index.js");
 
 if (isBrowser) {
-  const scriptTag = document.currentScript;
-  const apiKey = scriptTag.getAttribute("data-weploy-key");
-  const disableAutoTranslateAttr = scriptTag.getAttribute("data-disable-auto-translate")
-  const disableAutoTranslate = disableAutoTranslateAttr == "true";
-  getTranslations(apiKey, {disableAutoTranslate});
-  createLanguageSelect(apiKey);
+  document.addEventListener("DOMContentLoaded", function() {
+    const scriptTag = document.currentScript;
+    const apiKey = scriptTag.getAttribute("data-weploy-key");
+    const disableAutoTranslateAttr = scriptTag.getAttribute("data-disable-auto-translate")
+    const disableAutoTranslate = disableAutoTranslateAttr == "true";
+    getTranslations(apiKey, {disableAutoTranslate});
+    createLanguageSelect(apiKey);
+  });
 }
