2d5480b72e6a48d557bd79b7f96ce0283edea6eb
Lukas Seyfarth <jemikanegara@gmail.com>
2024-04-11 18:39:53 +0800

    new language selector

--- index.js
+++ index.js
@@ -8,9 +8,53 @@ var isDomListenerAdded;
 var weployOptions;
 const API_URL = "https://api.tasksource.io"
 
+
+function getLoadingGlobeIcon(strokeColor) {
+    // Create a new div element
+    var newDiv = document.createElement("div");
+
+    // Set the innerHTML of the div
+    newDiv.innerHTML = `<svg style="width:24px;height:24px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                            <circle cx="50" cy="50" fill="none" stroke="${strokeColor || 'currentColor'}" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138">
+                                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" values="0 50 50;360 50 50" keyTimes="0;1"></animateTransform>
+                            </circle>
+                        </svg>`;
+    return newDiv;
+}
+
+function renderWeployLoadingState() {
+  if (!window.weployValueDisplays.length) return;
+
+  window.weployValueDisplays.forEach((selector, selectorIndex) => {
+    const loadingIcon = window.weployLoadingGlobeIcons[selectorIndex];
+    const staticIcon = window.weployStaticGlobeIcons[selectorIndex];
+    const firstChild = selector.firstChild;
+
+    if (!firstChild || !staticIcon || !loadingIcon) {
+      return;
+    }
+
+    if (!selector.contains(staticIcon) && !selector.contains(loadingIcon)) {
+      const whatShouldBeInserted = window.weployTranslating ? loadingIcon : staticIcon;
+      selector.insertBefore(whatShouldBeInserted, firstChild);
+      return;
+    }
+
+    if (window.weployTranslating) {
+      selector.replaceChild(loadingIcon, staticIcon);
+      return;
+    }
+
+    selector.replaceChild(staticIcon, loadingIcon);
+  });
+}
+
 if (isBrowser) {
   window.translationCache = {}
   window.currentPathname = isBrowser ? window.location.pathname : null
+  window.weployValueDisplays = [];
+  window.weployStaticGlobeIcons = [];
+  window.weployLoadingGlobeIcons = [];
 }
 
 // initialize new event "pathnamechange"
@@ -255,7 +299,7 @@ function processTextNodes(textNodes, language = "", apiKey) {
         (response) => {
           notInCache.forEach((text, index) => {
             // Cache the new translations
-            window.translationCache[language][text] = response[index];
+            window.translationCache[language][text] = response[index] || text;
           });
           
           // Update textNodes from the cache
@@ -293,7 +337,13 @@ function modifyHtmlStrings(rootElement, language, apiKey) {
     extractTextNodes(rootElement, textNodes);
 
     const validTextNodes = filterValidTextNodes(textNodes);
-    await processTextNodes(validTextNodes, language, apiKey).catch(reject);
+
+    window.weployTranslating = true;
+    renderWeployLoadingState();
+    await processTextNodes(validTextNodes, language, apiKey).catch(reject).finally(() => {
+      window.weployTranslating = false;
+      renderWeployLoadingState();
+    });
 
     resolve();
   });
@@ -500,6 +550,9 @@ async function createLanguageSelect(apiKey) {
   }
 
   const availableLangs = await fetchLanguageList(apiKey);
+  const selectedLang = await getLanguageFromLocalStorage();
+  const selectedLangUppercased = (selectedLang || "").substring(0, 2).toUpperCase();
+  const selectedLangLowercased = (selectedLang || "").substring(0, 2).toLowerCase();
 
   ['weploy-select', 'weploy-select-with-name'].forEach(className => {
     const isWithLangLabel = className == "weploy-select-with-name";
@@ -512,44 +565,156 @@ async function createLanguageSelect(apiKey) {
     const weploySwitchers = Array.from(new Set([...classElements, ...idElements]));
 
     if (weploySwitchers.length > 0 && availableLangs && availableLangs.length > 0) {
-      weploySwitchers.forEach(weploySwitcher => {
+      weploySwitchers.forEach((weploySwitcher) => {
         // Create the select element if not already present
-        let selectElem = weploySwitcher.querySelector('select.weploy-exclude');
+        let selectElem = weploySwitcher.querySelector('.weploy-lang-selector-element');
         if (!selectElem) {
-          selectElem = document.createElement('select');
-          selectElem.className = "weploy-exclude";
-          selectElem.style = "border: none; background-color: transparent; cursor: pointer; outline: none;";
-          selectElem.onchange = (e) => {
-            const newValue = e.target.value;
-            // Update only the select elements within weploySwitchers
-            weploySwitchers.forEach(sw => {
-              const selects = sw.querySelector('select.weploy-exclude');
-              if (selects && selects !== e.target) {
-                selects.value = newValue;
-              }
-            });
-            switchLanguage(newValue);
-          };
+          let languages = availableLangs
+          
+          let div = document.createElement('div');
+          div.className = 'weploy-lang-selector-nav';
+          
+          let details = document.createElement('details');
+          details.dataset.behavior = 'languageSelector-topbar';
+          details.role = 'group';
+          details.className = "weploy-lang-selector-element"
+          div.appendChild(details);
+          
+          let summary = document.createElement('summary');
+          summary.setAttribute('aria-expanded', 'true');
+          summary.setAttribute('aria-haspopup', 'true');
+          summary.role = 'button';          
+          details.appendChild(summary);
 
-          // Populate the select element with options
-          availableLangs.forEach(async lang => {
-            const langOpts = document.createElement('option');
-            langOpts.value = lang.lang;
-            langOpts.textContent = isWithLangLabel ? `${lang.flag} ${lang.label}` : lang.flag;
-            // langOpts.style = "text-transform: uppercase;";
-            langOpts.selected = lang.lang === await getLanguageFromLocalStorage();
-
-            selectElem.appendChild(langOpts);
-          });
 
           // Check for "data-icon-color" attribute and use it for font color
           const iconColor = weploySwitcher.getAttribute('data-icon-color');
-          if (iconColor) {
-            selectElem.style.color = iconColor;
-          }
 
-          // Append the select element to each weploySwitcher
-          weploySwitcher.appendChild(selectElem);
+          let iconSvgInSummary = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+          iconSvgInSummary.setAttribute("fill", iconColor || "none");
+          iconSvgInSummary.setAttribute("height", "24");
+          iconSvgInSummary.setAttribute("viewBox", "0 0 24 24");
+          iconSvgInSummary.setAttribute("width", "24");
+
+          let iconPathInSummary = document.createElementNS("http://www.w3.org/2000/svg", "path");
+          iconPathInSummary.setAttribute("clip-rule", "evenodd");
+          iconPathInSummary.setAttribute("d", "m7.10319 18.721c1.44393.8125 3.11001 1.2768 4.88431 1.279-.2692 0-.4875.2183-.4875.4875v.5125c0 .5523-.4477 1-1 1h-3c-.27614 0-.5.2239-.5.5s.22386.5.50001.5h8.99999c.2761 0 .5-.2239.5-.5s-.2239-.5-.5-.5h-3c-.5523 0-1-.4477-1-1v-.5125c0-.2692-.2183-.4875-.4875-.4875 5.5171-.0068 9.9875-4.4813 9.9875-10.00001 0-3.6579-1.964-6.85708-4.895-8.60058-.231-.13742-.5264-.05181-.6608.18098-.1417.24537-.0501.55823.1926.70441 2.6145 1.57468 4.3632 4.44072 4.3632 7.71519 0 4.97061-4.0294 9.00001-9 9.00001-1.5829 0-3.07031-.4086-4.36257-1.1262-.248-.1377-.56522-.0607-.70705.1849-.13422.2325-.06112.5307.17281.6623zm4.89681-.721c4.4183 0 8-3.5817 8-8 0-4.41828-3.5817-8-8-8-4.41828 0-8 3.58172-8 8 0 4.4183 3.58172 8 8 8zm0-1c2.4116 0 4.5384-1.2195 5.7973-3.0756l-1.7255-.9962c-1.248 1.9812-2.9042 3.4124-4.5242 4.0574.1496.0096.3004.0144.4524.0144zm6.002-3.3957c.0436-.0724.0859-.1458.1269-.2199zm-1.4302-1.5421 1.7263.9967c.4496-.9242.7019-1.9621.7019-3.0589 0-1.43763-.4334-2.77403-1.1766-3.8857.2517 1.72562-.1596 3.87609-1.2516 5.9479zm-.6659-7.8275c1.4232 1.29546 1.3536 4.36603-.2005 7.3273l-3.0224-1.745zm-.866-.5-3.2229 5.5823-3.02242-1.74497c1.78752-2.8265 4.41182-4.4221 6.24532-3.83733zm-3.7229 6.4483-3.02242-1.74495c-1.55404 2.96125-1.62368 6.03175-.20051 7.32725zm1.1353-7.16862c-1.6199.64499-3.27617 2.07624-4.52413 4.05743l-1.72548-.99621c1.25887-1.85604 3.38567-3.0756 5.79731-3.0756.152 0 .3028.00484.4523.01438zm-6.5821 3.60277c.04159-.07519.08451-.14953.12875-.22299zm1.55798 1.32068-1.72627-.99665c-.44966.92412-.70191 1.96201-.70191 3.05882 0 1.4376.43339 2.774 1.17664 3.8857-.25177-1.7256.15954-3.8761 1.25154-5.94787zm7.77722 4.49017-3.0224-1.745-3.22291 5.5823c1.83351.5847 4.45781-1.0108 6.24531-3.8373z");
+          if (iconColor) iconPathInSummary.setAttribute("fill", iconColor);
+          iconPathInSummary.setAttribute("fill-rule", "evenodd");
+          iconSvgInSummary.appendChild(iconPathInSummary);
+          
+          window.weployValueDisplays.push(summary);
+          window.weployLoadingGlobeIcons.push(getLoadingGlobeIcon(iconColor));
+          window.weployStaticGlobeIcons.push(iconSvgInSummary);
+
+          renderWeployLoadingState();
+
+          let spanInSummary = document.createElement('span');
+          spanInSummary.setAttribute('aria-hidden', 'true');
+          spanInSummary.textContent = selectedLangUppercased;
+          spanInSummary.classList.add('weploy-lang-selector-value')
+          if (iconColor) {
+            spanInSummary.style.color = iconColor;
+          }
+          summary.appendChild(spanInSummary);
+
+          // Create a dropdown icon
+          const dropdownIcon = document.createElement("div");
+          dropdownIcon.style.width = ".25rem";
+          dropdownIcon.style.height = ".25rem";
+          dropdownIcon.style.backgroundColor = iconColor || "#241c15";
+          dropdownIcon.style.webkitClipPath = "polygon(50% 75%,100% 25%,0 25%)";
+          dropdownIcon.style.clipPath = "polygon(50% 75%,100% 25%,0 25%)";
+          summary.appendChild(dropdownIcon);
+
+          let ul = document.createElement('ul');
+          ul.className = 'weploy-lang-selector-menu-container';
+          details.appendChild(ul);
+          
+          languages.forEach((language, index) => {
+              const isSelected = language.lang == selectedLangLowercased;
+
+              let li = document.createElement('li');
+              ul.appendChild(li);
+
+              let a = document.createElement('a');
+              const url = isSelected ? "#" : addOrReplaceLangParam(window.location.href, language.lang);
+
+              a.href = url;
+              a.hreflang = language.lang;
+              a.dataset.locale = language.lang;
+              a.dataset.index = index;
+
+              a.className = `weploy-lang-selector-menu-container-item ${isSelected ? 'selected' : ''}`;
+
+              li.appendChild(a);
+          
+              let span = document.createElement('span');
+              span.setAttribute('aria-hidden', 'true');
+              span.className = 'weploy-lang-selector-menu-container-item-code';
+              span.textContent = language.lang.toUpperCase();
+              a.appendChild(span);
+          
+              let p = document.createElement('p');
+              p.lang = language.lang;
+              p.className = 'weploy-lang-selector-menu-container-item-locale';
+              p.textContent = language.label;
+              a.appendChild(p);
+
+              if (isSelected) {
+                let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+                svg.setAttribute('aria-hidden', 'true');
+                svg.setAttribute('viewBox', '0 0 24 24');
+                svg.setAttribute('class', 'weploy-lang-selector-menu-container-item-selected');
+                a.appendChild(svg);
+        
+                let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                path.setAttribute('fill-rule', 'evenodd');
+                path.setAttribute('clip-rule', 'evenodd');
+                path.setAttribute('d', 'M20.7071 7.70712L9.99995 18.4142L4.79285 13.2071L6.20706 11.7929L9.99995 15.5858L19.2928 6.29291L20.7071 7.70712Z');
+                svg.appendChild(path);
+              }
+          });
+          
+          weploySwitcher.appendChild(div);
+          weploySwitcher.classList.add('weploy-lang-selector-wrapper')
+          weploySwitcher.classList.add('weploy-exclude')
+
+          //////////
+          // selectElem = document.createElement('select');
+          // selectElem.className = "weploy-exclude";
+          // selectElem.style = "border: none; background-color: transparent; cursor: pointer; outline: none;";
+          // selectElem.onchange = (e) => {
+          //   const newValue = e.target.value;
+          //   // Update only the select elements within weploySwitchers
+          //   weploySwitchers.forEach(sw => {
+          //     const selects = sw.querySelector('select.weploy-exclude');
+          //     if (selects && selects !== e.target) {
+          //       selects.value = newValue;
+          //     }
+          //   });
+          //   switchLanguage(newValue);
+          // };
+
+          // // Populate the select element with options
+          // availableLangs.forEach(async lang => {
+          //   const langOpts = document.createElement('option');
+          //   langOpts.value = lang.lang;
+          //   langOpts.textContent = isWithLangLabel ? `${lang.flag} ${lang.label}` : lang.flag;
+          //   // langOpts.style = "text-transform: uppercase;";
+          //   langOpts.selected = lang.lang === await getLanguageFromLocalStorage();
+
+          //   selectElem.appendChild(langOpts);
+          // });
+
+          // // Check for "data-icon-color" attribute and use it for font color
+          // const iconColor = weploySwitcher.getAttribute('data-icon-color');
+          // if (iconColor) {
+          //   selectElem.style.color = iconColor;
+          // }
+
+          // // Append the select element to each weploySwitcher
+          // weploySwitcher.appendChild(selectElem);
         }
       });
     }
