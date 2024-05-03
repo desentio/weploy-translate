e150c06102fb11f9f86442e126b2210b38e0399e
Lukas Seyfarth <luki.seyfarth@web.de>
2024-05-03 15:59:44 +0200

    asd

--- utils/languages/allWeployLanguagesList.js
+++ utils/languages/allWeployLanguagesList.js
@@ -10,6 +10,7 @@ const allWeployLanguagesList = [
   { "label": "Belarusian", "lang": "be" },
   { "label": "Bulgarian", "lang": "bg" },
   { "label": "Bambara", "lang": "bm" },
+  { "label": "Kazakh", "lang": "kk" },
   { "label": "Bislama", "lang": "bi" },
   { "label": "Bengali", "lang": "bn" },
   { "label": "Tibetan", "lang": "bo" },
