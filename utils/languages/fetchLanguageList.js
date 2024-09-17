d2e87500b9255547f75312d9e2ada0a4009a2d65
jemikanegara <jemikanegara@gmail.com>
2024-09-17 18:40:36 +0800

    hardcode special api key

--- utils/languages/fetchLanguageList.js
+++ utils/languages/fetchLanguageList.js
@@ -9,45 +9,77 @@ async function fetchLanguageList(window, apiKey) {
   if (window.globalseoError) return [];
   if (!SPECIAL_API_KEYS.includes(options.apiKey)) return [];
 
-  const shouldCompressResponse = isCompressionSupported(window);
-  const headers = {
-    "X-Api-Key": apiKey,
+  const res = {
+    "Id": 142,
+    "name": "манжерок.рф",
+    "language": "ru",
+    "allowedLanguages": [
+        "en",
+        "zh"
+    ],
+    "flags": [
+        "🇷🇺",
+        "🇺🇸",
+        "🇨🇳"
+    ],
+    "labels": [
+        "Russian",
+        "English",
+        "Chinese"
+    ]
   }
 
-  if (shouldCompressResponse) {
-    headers["Accept"] = "application/octet-stream"; // to receive compressed response
-  }
+  const languages =  [res.language, ...res.allowedLanguages]
+  const languagesWithFlagAndLabel = languages.map((lang, index) => ({
+    lang,
+    flag: (res.flags || [])?.[index] || lang, // fallback to text if flag unavailable
+    label: (res.labels || [])?.[index] || lang // fallback to text if flag unavailable
+  }))
 
-  const availableLangs = await window.fetch(API_URL + "/globalseo-projects/by-api-key", {
-    headers
-  })
-  .then((response) => shouldCompressResponse ? response.arrayBuffer() : response.json())
-  .then(data => shouldCompressResponse ? decompressArrayBuffer(window, data, "gzip") : data)
-  .then(data => shouldCompressResponse ? JSON.parse(data) : data)
-  .then((res) => {
-    if (res.error) {
-      throw new Error(res.error?.message || res.error || "Error fetching languages")
-    }
-    const languages =  [res.language, ...res.allowedLanguages]
-    const languagesWithFlagAndLabel = languages.map((lang, index) => ({
-      lang,
-      flag: (res.flags || [])?.[index] || lang, // fallback to text if flag unavailable
-      label: (res.labels || [])?.[index] || lang // fallback to text if flag unavailable
-    }))
-    setGlobalseoOptions(window, { definedLanguages: languagesWithFlagAndLabel })
-    setGlobalseoActiveLang(window, languagesWithFlagAndLabel[0].lang)
-    return languagesWithFlagAndLabel
-  })
-  .catch((err) => {
-    console.error(err);
-    // if (isBrowser()) window.globalseoOptions.definedLanguages = [] // store in global scope
-    // else globalseoOptions.definedLanguages = [] // for npm package
-    window.globalseoError = err.message;
-    renderSelectorState(window, { shouldUpdateActiveLang: false });
-    return [];
-  })
+  setGlobalseoOptions(window, { definedLanguages: languagesWithFlagAndLabel })
+  setGlobalseoActiveLang(window, languagesWithFlagAndLabel[0].lang)
+  return languagesWithFlagAndLabel
 
-  return availableLangs
+  // const shouldCompressResponse = isCompressionSupported(window);
+  // const headers = {
+  //   "X-Api-Key": "cb20c25d-2d15-46ab-aac2-de5aa6b0aeda",
+  // }
+
+  // if (shouldCompressResponse) {
+  //   headers["Accept"] = "application/octet-stream"; // to receive compressed response
+  // }
+
+  // const availableLangs = await window.fetch(API_URL + "/globalseo-projects/by-api-key", {
+  //   headers
+  // })
+  // .then((response) => shouldCompressResponse ? response.arrayBuffer() : response.json())
+  // .then(data => shouldCompressResponse ? decompressArrayBuffer(window, data, "gzip") : data)
+  // .then(data => shouldCompressResponse ? JSON.parse(data) : data)
+  // .then((res) => {
+  //   if (res.error) {
+  //     throw new Error(res.error?.message || res.error || "Error fetching languages")
+  //   }
+  //   const languages =  [res.language, ...res.allowedLanguages]
+  //   const languagesWithFlagAndLabel = languages.map((lang, index) => ({
+  //     lang,
+  //     flag: (res.flags || [])?.[index] || lang, // fallback to text if flag unavailable
+  //     label: (res.labels || [])?.[index] || lang // fallback to text if flag unavailable
+  //   }))
+  //   console.log("languagesWithFlagAndLabel", res)
+  //   setGlobalseoOptions(window, { definedLanguages: languagesWithFlagAndLabel })
+  //   setGlobalseoActiveLang(window, languagesWithFlagAndLabel[0].lang)
+  //   return languagesWithFlagAndLabel
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  //   // if (isBrowser()) window.globalseoOptions.definedLanguages = [] // store in global scope
+  //   // else globalseoOptions.definedLanguages = [] // for npm package
+  //   window.globalseoError = err.message;
+  //   renderSelectorState(window, { shouldUpdateActiveLang: false });
+  //   return [];
+  // })
+
+  // return availableLangs
 }
 
 module.exports = {
