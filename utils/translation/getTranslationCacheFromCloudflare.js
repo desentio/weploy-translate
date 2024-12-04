ae84ef0f8d562d86dc242a6f19d703ea8700fa07
jemikanegara <jemikanegara@gmail.com>
2024-12-04 21:11:21 +0800

    disable cdn on worker

--- utils/translation/getTranslationCacheFromCloudflare.js
+++ utils/translation/getTranslationCacheFromCloudflare.js
@@ -4,7 +4,7 @@ const getTranslationCacheFromCDN = require("./getTranslationCacheFromCDN");
 const getTranslationCacheFromKV = require("./getTranslationCacheFromKV");
 
 async function getTranslationCacheFromCloudflare(window, language, apiKey) {
-  if (!isCompressionSupported(window)) return {};
+  if (!isCompressionSupported(window) || window.isWorker) return {};
   if (window?.cloudflareCache) return window?.cloudflareCache;
 
   const isUsingKV = true;
